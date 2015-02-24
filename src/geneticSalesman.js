d4b8f198f594ccf3996356732aa183cfa5238bee
riahk <moar.riah@gmail.com>
2015-02-23 21:35:57 -0800

    Algorithm works

--- src/geneticSalesman.js
+++ src/geneticSalesman.js
@@ -1,12 +1,36 @@
 var geneticSalesman = function(genes, assessFitness, initiateBloodline, mutate, availableResources){
   var options = {
     numberOfBloodlines: 10,
-    offspringPerSurvivor: 50,
+    offspringPerSurvivor: 30
   };
 
   /* -------------------- Complete me! -------------------- */
+  var offspring = [];
+  for(var i = 0; i < options.offspringPerSurvivor ; i++) {
+    var route = createRoute(genes);
+    offspring.push(route);
+  }
 
-  return optimalRoute;
+  var optimalOffspring = offspring[0];
+
+  for(var j = 0; j < availableResources ; j++) {
+    optimalOffspring = offspring[0];
+    for(var k = 0; k < offspring.length ; k++) {
+      var distance = assessFitness(offspring[k]);
+      var optDistance = assessFitness(optimalOffspring);
+      if( distance <= optDistance ) {
+        optimalOffspring = offspring[k];
+      }
+    }
+
+    for(var m = 0; m < offspring.length ; m++) {
+      var mutation = mutate(optimalOffspring);
+      offspring[m] = mutation;
+    }
+
+  }
+
+  return optimalOffspring;
 }
 
 var createRoute = function(cities){
@@ -22,8 +46,20 @@ var createRoute = function(cities){
 var alterRoute = function(route){
 
   /* -------------------- Complete me! -------------------- */
+  var newRoute = route.slice();
+  var randomIndex1 = Math.floor(Math.random() * route.length);
+  var randomIndex2 = Math.floor(Math.random() * route.length);
+  
+  var city1 = newRoute[randomIndex1];
+  var city2 = newRoute[randomIndex2];
+  var temp = newRoute[randomIndex1];
 
-}
+  newRoute[randomIndex1] = newRoute[randomIndex2];
+  newRoute[randomIndex2] = temp;
+
+  return newRoute;
+
+};
 
 var calculateDistance = function(route){
   var distances = route.map(function(city, index, route){
